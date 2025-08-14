14aef0cbcc92c6b2371427e959592d78d9d54315
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-14 13:52:35 +0530

    Create 03_Clear Day.cpp

---
+++ 03_Clear Day.cpp
@@ -0,0 +1,16 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int x, y;
+	cin>>x>>y;
+	
+	int clear_day = 7 - (x+y);
+	
+	cout<<clear_day<<endl;
+
+}
