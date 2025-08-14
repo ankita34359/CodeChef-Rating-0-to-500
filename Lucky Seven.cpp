8affb554c0c02b124cfc495674fdd22365815032
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-14 13:50:49 +0530

    Create Lucky Seven.cpp

---
+++ Lucky Seven.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	string s;
+	cin>>s;
+	
+	cout<<s[6];
+
+}
