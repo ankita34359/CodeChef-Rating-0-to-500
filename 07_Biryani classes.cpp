80dd687b47027db3bf0e6e1469412f829ec82c98
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:04:05 +0530

    Update and rename 07_Masterchef finals.cpp to 07_Biryani classes.cpp

---
+++ 07_Biryani classes.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    
+	    cout<<x*y<<endl;
+	}
+
+
+}
