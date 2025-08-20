a96b85ad3d2232733bce868818faa56c4a7c9dfd
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:10:17 +0530

    Create 13_Donation Drive.cpp

---
+++ 13_Donation Drive.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DONDRIVE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n, x;
+	    cin>>n>>x;
+	    
+	    cout<<n-x<<endl;
+	}
+
+}
