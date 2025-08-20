378176cab51087a663623dae4ffcbfb95bc8d9da
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:21:36 +0530

    Create 19_Bone Appetit.cpp

---
+++ 19_Bone Appetit.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n, m;
+	cin>>n>>m;
+	
+	int x, y;
+	cin>>x>>y;
+	
+	cout<<n*x+m*y<<endl;
+
+}
