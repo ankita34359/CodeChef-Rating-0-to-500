beccfc40bcde6aab6ec2e95100e6bca24f6a4818
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:10:54 +0530

    Create 14_Kitchen Timings.cpp

---
+++ 14_Kitchen Timings.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/KITCHENTIME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    
+	    cout<<y-x<<endl;
+	}
+
+}
