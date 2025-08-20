668015fb7818d1f3bb59d414bc07cf498fff33ab
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:07:39 +0530

    Create 10_Determine the Score.cpp

---
+++ 10_Determine the Score.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x, n;
+	    cin>>x>>n;
+	    
+	    int a = x/10;
+	    
+	    cout<<a*n<<endl;
+	}
+
+}
