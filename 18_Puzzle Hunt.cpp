97745e7bee21543b9320a562db673c78e06850f7
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:20:35 +0530

    Create 18_Puzzle Hunt.cpp

---
+++ 18_Puzzle Hunt.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int n;
+	cin>>n;
+	
+	if(n>=6 && n<=8){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
