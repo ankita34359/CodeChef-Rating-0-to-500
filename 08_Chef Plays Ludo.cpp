505860c465e3479e7bba07d73fe245076d7053e5
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:05:13 +0530

    Create 08_Chef Plays Ludo.cpp

---
+++ 08_Chef Plays Ludo.cpp
@@ -0,0 +1,21 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUDO
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x;
+	    cin>>x;
+	    
+	    if(x==6){
+	        cout<<"YES"<<endl
+	    }
+	}
+
+}
