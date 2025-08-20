c292a693d8823de4e5a79277c4b75e20fe3af0ae
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:09:19 +0530

    Create 12_Off By One.cpp

---
+++ 12_Off By One.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int a, b;
+	cin>>a>>b;
+	
+	cout<<a+b<<1<<endl;
+
+
+}
