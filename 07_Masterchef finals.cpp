f3cf1686a204e356a3837fb4ce8d0d1b7b900fb9
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-08-20 13:00:04 +0530

    Create 07_Masterchef finals.cpp

---
+++ 07_Masterchef finals.cpp
@@ -0,0 +1,24 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TOP10
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x;
+	    cin>>x;
+	    
+	    if(x>10){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+
+}
